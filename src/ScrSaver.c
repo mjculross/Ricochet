d6c06acc618a05c2f61771610a4f132c3d2c38e5
mjculross <mjculross@sbcglobal.net>
2013-05-25 20:06:50 -0500

    ScrSaver v1.2 pebble app/watchface

--- src/ScrSaver.c
+++ src/ScrSaver.c
@@ -4,7 +4,7 @@
 #define MY_UUID {0x56, 0x91, 0x05, 0x7a, 0xda, 0x28, 0x4c, 0xee, 0x94, 0x09, 0x74, 0x7c, 0x5c, 0x2d, 0x3d, 0x95}
 PBL_APP_INFO(MY_UUID,
 	"ScrSaver", "Pebble Technology & KD5RXT",
-	1, 1, /* App major/minor version */
+	1, 2, /* App major/minor version */
 	RESOURCE_ID_IMAGE_MENU_ICON,
 	APP_INFO_STANDARD_APP);
 
@@ -14,14 +14,13 @@ Window window;
 #define TOTAL_TIME_DIGITS 6
 #define TOTAL_DATE_DIGITS 8
 
-typedef enum {TIME_ON_TOP_MODE = 0, DATE_ON_TOP_MODE, MODE_COUNT} DISPLAY_MODE;
-
-int display_mode = TIME_ON_TOP_MODE;
-
 bool night_enabled = false;
 bool clock_24h_style;
+bool date_month_first = true;
+bool time_on_top = true;
 
 int splash_timer = 3;
+int freeze_timer;
 
 int time_x_max;
 
@@ -135,8 +134,9 @@ void select_long_release_handler(ClickRecognizerRef recognizer, Window *window);
 void select_single_click_handler(ClickRecognizerRef recognizer, Window *window);
 void set_container_image(BmpContainer *bmp_container, const int resource_id, GPoint origin);
 void up_single_click_handler(ClickRecognizerRef recognizer, Window *window);
-void update_date(PblTm *current_time);
-void update_time(PblTm *current_time);
+void update_date(PblTm *current_time, bool move);
+void update_display(PblTm *current_time, bool move);
+void update_time(PblTm *current_time, bool move);
 
 
 void click_config_provider(ClickConfig **config, Window *window)
@@ -158,8 +158,24 @@ void click_config_provider(ClickConfig **config, Window *window)
 
 void down_single_click_handler(ClickRecognizerRef recognizer, Window *window)
 {
+   PblTm current_time;
+
    (void)recognizer;
    (void)window;
+
+   clock_24h_style = !clock_24h_style;
+
+   if (clock_24h_style)
+   {
+      time_x_max = 93;
+   }
+   else
+   {
+      time_x_max = 103;
+   }
+
+   get_time(&current_time);
+   update_display(&current_time, false);
 }  // down_single_click_handler()
 
 
@@ -182,19 +198,17 @@ void handle_deinit(AppContextRef ctx)
 
 void handle_init(AppContextRef ctx)
 {
-   PblTm time_now;
-
-   get_time(&time_now);
    (void)ctx;
 
    // version 1.1 of SDK requires vars to be intialized manually
    // START manual var intialization
 
-   display_mode = TIME_ON_TOP_MODE;
-
    night_enabled = false;
+   date_month_first = true;
+   time_on_top = true;
 
    splash_timer = 3;
+   freeze_timer = 5;
 
    if (clock_is_24h_style())
    {
@@ -207,18 +221,18 @@ void handle_init(AppContextRef ctx)
       time_x_max = 103;
    }
 
-   time_x_offset = 20;
-   time_y_offset = 75;
+   time_x_offset = 0;
+   time_y_offset = 0;
 
    time_x_delta = 2;
    time_y_delta = 3;
 
    date_x_max = 104;
    date_x_offset = 0;
-   date_y_offset = 10;
+   date_y_offset = 0;
 
-   date_x_delta = -2;
-   date_y_delta = -3;
+   date_x_delta = -3;
+   date_y_delta = -2;
    // END manual var intialization
 
    window_init(&window, "ScrSaver");
@@ -268,23 +282,18 @@ void handle_second_tick(AppContextRef ctx, PebbleTickEvent *t)
       }
    }
 
-   switch (display_mode)
+   if (freeze_timer > 0)
    {
-      case TIME_ON_TOP_MODE:
-         update_date(t->tick_time);
-         update_time(t->tick_time);
-      break;
-
-      case DATE_ON_TOP_MODE:
-         update_time(t->tick_time);
-         update_date(t->tick_time);
-      break;
+      freeze_timer--;
    }
+   update_display(t->tick_time, true);
 }  // handle_second_tick()
 
 
 void select_long_click_handler(ClickRecognizerRef recognizer, Window *window)
 {
+   PblTm current_time;
+
    (void)recognizer;
    (void)window;
 
@@ -292,6 +301,11 @@ void select_long_click_handler(ClickRecognizerRef recognizer, Window *window)
    {
       night_enabled = !night_enabled;
    }
+
+   freeze_timer = 5;
+
+   get_time(&current_time);
+   update_display(&current_time, false);
 }  // select_long_click_handler()
 
 
@@ -304,14 +318,17 @@ void select_long_release_handler(ClickRecognizerRef recognizer, Window *window)
 
 void select_single_click_handler(ClickRecognizerRef recognizer, Window *window)
 {
+   PblTm current_time;
+
    (void)recognizer;
    (void)window;
 
-   display_mode++;
-   if (display_mode == MODE_COUNT)
-   {
-      display_mode = TIME_ON_TOP_MODE;
-   }
+   time_on_top = !time_on_top;
+
+   freeze_timer = 5;
+
+   get_time(&current_time);
+   update_display(&current_time, false);
 }  // select_single_click_handler()
 
 
@@ -333,132 +350,214 @@ void set_container_image(BmpContainer *bmp_container, const int resource_id, GPo
 
 void up_single_click_handler(ClickRecognizerRef recognizer, Window *window)
 {
+   PblTm current_time;
+
    (void)recognizer;
    (void)window;
+
+   date_month_first = !date_month_first;
+
+   get_time(&current_time);
+   update_display(&current_time, false);
 }  // up_single_click_handler()
 
 
-void update_date(PblTm *current_time)
+void update_date(PblTm *current_time, bool move)
 {
    float x, y;
 
-   // total date field is 104w x 39h
-   if ((date_x_offset + date_x_delta) < 0)
+   if (freeze_timer > 0)
    {
-      // generate a pseudo random number from 2, 4, & 6
-      x =  (sin_lookup (current_time->tm_hour + current_time->tm_min + current_time->tm_sec + 3)) * 1000;
-      date_x_delta = (int) x;
-      date_x_delta = ((date_x_delta % 3) + 1) * 2;
+      if (time_on_top)
+      {
+         date_x_offset = 20;
+         date_y_offset = 75;
+      }
+      else
+      {
+         date_x_offset = 20;
+         date_y_offset = 10;
+      }
    }
    else
    {
-      if ((date_x_offset + date_x_delta + date_x_max) >=144)
+      if (move)
       {
-         // generate a pseudo random number from -2, -4, & -6
-         x =  (cos_lookup (current_time->tm_hour + current_time->tm_min + current_time->tm_sec + 7)) * 1000;
-         date_x_delta = (int) x;
-         date_x_delta = ((date_x_delta % 3) + 1) * 2;
-         date_x_delta = -date_x_delta;
+         // total date field is 104w x 39h
+         if ((date_x_offset + date_x_delta) < 0)
+         {
+            // generate a pseudo random number from 2, 4, & 6
+            x =  (sin_lookup (current_time->tm_hour + current_time->tm_min + current_time->tm_sec + 3)) * 1000;
+            date_x_delta = (int) x;
+            date_x_delta = ((date_x_delta % 3) + 1) * 2;
+         }
+         else
+         {
+            if ((date_x_offset + date_x_delta + date_x_max) >=144)
+            {
+               // generate a pseudo random number from -2, -4, & -6
+               x =  (cos_lookup (current_time->tm_hour + current_time->tm_min + current_time->tm_sec + 7)) * 1000;
+               date_x_delta = (int) x;
+               date_x_delta = ((date_x_delta % 3) + 1) * 2;
+               date_x_delta = -date_x_delta;
+            }
+         }
+
+         if ((date_y_offset + date_y_delta) < 0)
+         {
+            // generate a pseudo random number from 4, 8, & 12
+            y =  (cos_lookup (current_time->tm_hour + current_time->tm_min + current_time->tm_sec + 9)) * 1000;
+            date_y_delta = (int) y;
+            date_y_delta = ((date_y_delta % 3) + 1) * 4;
+         }
+         else
+         {
+            if ((date_y_offset + date_y_delta + 41) >= 168)
+            {
+               // generate a pseudo random number from -4, -8, & -12
+               y =  (sin_lookup (current_time->tm_hour + current_time->tm_min + current_time->tm_sec + 11)) * 1000;
+               date_y_delta = (int) y;
+               date_y_delta = ((date_y_delta % 3) + 1) * 4;
+               date_y_delta = -date_y_delta;
+            }
+         }
+
+         date_x_offset += date_x_delta;
+         date_y_offset += date_y_delta;
       }
    }
-
-   if ((date_y_offset + date_y_delta) < 0)
-   {
-      // generate a pseudo random number from 4, 8, & 12
-      y =  (cos_lookup (current_time->tm_hour + current_time->tm_min + current_time->tm_sec + 9)) * 1000;
-      date_y_delta = (int) y;
-      date_y_delta = ((date_y_delta % 3) + 1) * 4;
-   }
-   else
-   {
-      if ((date_y_offset + date_y_delta + 41) >= 168)
-      {
-         // generate a pseudo random number from -4, -8, & -12
-         y =  (sin_lookup (current_time->tm_hour + current_time->tm_min + current_time->tm_sec + 11)) * 1000;
-         date_y_delta = (int) y;
-         date_y_delta = ((date_y_delta % 3) + 1) * 4;
-         date_y_delta = -date_y_delta;
-      }
-   }
-
-   date_x_offset += date_x_delta;
-   date_y_offset += date_y_delta;
 
    // display date
    if (night_enabled == false)
    {
       set_container_image(&day_image, DAY_IMAGE_RESOURCE_IDS[current_time->tm_wday], GPoint(date_x_offset + 30, date_y_offset));
 
-      set_container_image(&date_images[0], DATENUM_IMAGE_RESOURCE_IDS[(current_time->tm_mon + 1) / 10], GPoint(date_x_offset, date_y_offset + 23));
-      set_container_image(&date_images[1], DATENUM_IMAGE_RESOURCE_IDS[(current_time->tm_mon + 1) % 10], GPoint(date_x_offset + 13, date_y_offset + 23));
-      set_container_image(&date_images[2], RESOURCE_ID_IMAGE_DATENUM_SLASH, GPoint(date_x_offset + 26, date_y_offset + 23));
-      set_container_image(&date_images[3], DATENUM_IMAGE_RESOURCE_IDS[current_time->tm_mday / 10], GPoint(date_x_offset + 39, date_y_offset + 23));
-      set_container_image(&date_images[4], DATENUM_IMAGE_RESOURCE_IDS[current_time->tm_mday % 10], GPoint(date_x_offset + 52, date_y_offset + 23));
-      set_container_image(&date_images[5], RESOURCE_ID_IMAGE_DATENUM_SLASH, GPoint(date_x_offset + 65, date_y_offset + 23));
+      if (date_month_first)
+      {
+         set_container_image(&date_images[0], DATENUM_IMAGE_RESOURCE_IDS[(current_time->tm_mon + 1) / 10], GPoint(date_x_offset, date_y_offset + 23));
+         set_container_image(&date_images[1], DATENUM_IMAGE_RESOURCE_IDS[(current_time->tm_mon + 1) % 10], GPoint(date_x_offset + 13, date_y_offset + 23));
+         set_container_image(&date_images[3], DATENUM_IMAGE_RESOURCE_IDS[current_time->tm_mday / 10], GPoint(date_x_offset + 39, date_y_offset + 23));
+         set_container_image(&date_images[4], DATENUM_IMAGE_RESOURCE_IDS[current_time->tm_mday % 10], GPoint(date_x_offset + 52, date_y_offset + 23));
+      }
+      else
+      {
+         set_container_image(&date_images[3], DATENUM_IMAGE_RESOURCE_IDS[current_time->tm_mday / 10], GPoint(date_x_offset, date_y_offset + 23));
+         set_container_image(&date_images[4], DATENUM_IMAGE_RESOURCE_IDS[current_time->tm_mday % 10], GPoint(date_x_offset + 13, date_y_offset + 23));
+         set_container_image(&date_images[0], DATENUM_IMAGE_RESOURCE_IDS[(current_time->tm_mon + 1) / 10], GPoint(date_x_offset + 39, date_y_offset + 23));
+         set_container_image(&date_images[1], DATENUM_IMAGE_RESOURCE_IDS[(current_time->tm_mon + 1) % 10], GPoint(date_x_offset + 52, date_y_offset + 23));
+      }
+
       set_container_image(&date_images[6], DATENUM_IMAGE_RESOURCE_IDS[(current_time->tm_year / 10) % 10], GPoint(date_x_offset + 78, date_y_offset + 23));
       set_container_image(&date_images[7], DATENUM_IMAGE_RESOURCE_IDS[current_time->tm_year % 10], GPoint(date_x_offset + 91, date_y_offset + 23));
+      set_container_image(&date_images[2], RESOURCE_ID_IMAGE_DATENUM_SLASH, GPoint(date_x_offset + 26, date_y_offset + 23));
+      set_container_image(&date_images[5], RESOURCE_ID_IMAGE_DATENUM_SLASH, GPoint(date_x_offset + 65, date_y_offset + 23));
    }
    else
    {
       set_container_image(&day_image, DAY_INV_IMAGE_RESOURCE_IDS[current_time->tm_wday], GPoint(date_x_offset + 30, date_y_offset));
 
-      set_container_image(&date_images[0], DATENUM_INV_IMAGE_RESOURCE_IDS[(current_time->tm_mon + 1) / 10], GPoint(date_x_offset, date_y_offset + 23));
-      set_container_image(&date_images[1], DATENUM_INV_IMAGE_RESOURCE_IDS[(current_time->tm_mon + 1) % 10], GPoint(date_x_offset + 13, date_y_offset + 23));
-      set_container_image(&date_images[2], RESOURCE_ID_IMAGE_DATENUM_INV_SLASH, GPoint(date_x_offset + 26, date_y_offset + 23));
-      set_container_image(&date_images[3], DATENUM_INV_IMAGE_RESOURCE_IDS[current_time->tm_mday / 10], GPoint(date_x_offset + 39, date_y_offset + 23));
-      set_container_image(&date_images[4], DATENUM_INV_IMAGE_RESOURCE_IDS[current_time->tm_mday % 10], GPoint(date_x_offset + 52, date_y_offset + 23));
-      set_container_image(&date_images[5], RESOURCE_ID_IMAGE_DATENUM_INV_SLASH, GPoint(date_x_offset + 65, date_y_offset + 23));
+      if (date_month_first)
+      {
+         set_container_image(&date_images[0], DATENUM_INV_IMAGE_RESOURCE_IDS[(current_time->tm_mon + 1) / 10], GPoint(date_x_offset, date_y_offset + 23));
+         set_container_image(&date_images[1], DATENUM_INV_IMAGE_RESOURCE_IDS[(current_time->tm_mon + 1) % 10], GPoint(date_x_offset + 13, date_y_offset + 23));
+         set_container_image(&date_images[3], DATENUM_INV_IMAGE_RESOURCE_IDS[current_time->tm_mday / 10], GPoint(date_x_offset + 39, date_y_offset + 23));
+         set_container_image(&date_images[4], DATENUM_INV_IMAGE_RESOURCE_IDS[current_time->tm_mday % 10], GPoint(date_x_offset + 52, date_y_offset + 23));
+      }
+      else
+      {
+         set_container_image(&date_images[3], DATENUM_INV_IMAGE_RESOURCE_IDS[current_time->tm_mday / 10], GPoint(date_x_offset, date_y_offset + 23));
+         set_container_image(&date_images[4], DATENUM_INV_IMAGE_RESOURCE_IDS[current_time->tm_mday % 10], GPoint(date_x_offset + 13, date_y_offset + 23));
+         set_container_image(&date_images[0], DATENUM_INV_IMAGE_RESOURCE_IDS[(current_time->tm_mon + 1) / 10], GPoint(date_x_offset + 39, date_y_offset + 23));
+         set_container_image(&date_images[1], DATENUM_INV_IMAGE_RESOURCE_IDS[(current_time->tm_mon + 1) % 10], GPoint(date_x_offset + 52, date_y_offset + 23));
+      }
+
       set_container_image(&date_images[6], DATENUM_INV_IMAGE_RESOURCE_IDS[(current_time->tm_year / 10) % 10], GPoint(date_x_offset + 78, date_y_offset + 23));
       set_container_image(&date_images[7], DATENUM_INV_IMAGE_RESOURCE_IDS[current_time->tm_year % 10], GPoint(date_x_offset + 91, date_y_offset + 23));
+      set_container_image(&date_images[2], RESOURCE_ID_IMAGE_DATENUM_INV_SLASH, GPoint(date_x_offset + 26, date_y_offset + 23));
+      set_container_image(&date_images[5], RESOURCE_ID_IMAGE_DATENUM_INV_SLASH, GPoint(date_x_offset + 65, date_y_offset + 23));
    }
 }  // update_date()
 
 
-void update_time(PblTm *current_time)
+void update_display(PblTm *current_time, bool move)
+{
+   if (time_on_top)
+   {
+      update_date(current_time, move);
+      update_time(current_time, move);
+   }
+   else
+   {
+      update_time(current_time, move);
+      update_date(current_time, move);
+   }
+}  // update_display()
+
+
+void update_time(PblTm *current_time, bool move)
 {
    float x, y;
 
-   // total time field is 103w x 52h for 12-hour clock & 93w x 52h for 24-hour clock
-   if ((time_x_offset + time_x_delta) < 0)
+   if (freeze_timer > 0)
    {
-      // generate a pseudo random number from 2, 4, & 6
-      x =  (sin_lookup (current_time->tm_hour + current_time->tm_min + current_time->tm_sec + 19)) * 1000;
-      time_x_delta = (int) x;
-      time_x_delta = ((time_x_delta % 3) + 1) * 2;
+      if (time_on_top)
+      {
+         time_x_offset = 20;
+         time_y_offset = 10;
+      }
+      else
+      {
+         time_x_offset = 20;
+         time_y_offset = 75;
+      }
    }
    else
    {
-      if ((time_x_offset + time_x_delta + time_x_max) >=144)
+      if (move)
       {
-         // generate a pseudo random number from -2, -4, & -6
-         x =  (cos_lookup (current_time->tm_hour + current_time->tm_min + current_time->tm_sec + 37)) * 1000;
-         time_x_delta = (int) x;
-         time_x_delta = ((time_x_delta % 3) + 1) * 2;
-         time_x_delta = -time_x_delta;
+         // total time field is 103w x 52h for 12-hour clock & 93w x 52h for 24-hour clock
+         if ((time_x_offset + time_x_delta) < 0)
+         {
+            // generate a pseudo random number from 2, 4, & 6
+            x =  (sin_lookup (current_time->tm_hour + current_time->tm_min + current_time->tm_sec + 19)) * 1000;
+            time_x_delta = (int) x;
+            time_x_delta = ((time_x_delta % 3) + 1) * 2;
+         }
+         else
+         {
+            if ((time_x_offset + time_x_delta + time_x_max) >=144)
+            {
+               // generate a pseudo random number from -2, -4, & -6
+               x =  (cos_lookup (current_time->tm_hour + current_time->tm_min + current_time->tm_sec + 37)) * 1000;
+               time_x_delta = (int) x;
+               time_x_delta = ((time_x_delta % 3) + 1) * 2;
+               time_x_delta = -time_x_delta;
+            }
+         }
+
+         if ((time_y_offset + time_y_delta) < 0)
+         {
+            // generate a pseudo random number from 3, 6, & 9
+            y =  (cos_lookup (current_time->tm_hour + current_time->tm_min + current_time->tm_sec + 43)) * 1000;
+            time_y_delta = (int) y;
+            time_y_delta = ((time_y_delta % 3) + 1) * 3;
+         }
+         else
+         {
+            if ((time_y_offset + time_y_delta + 52) >= 168)
+            {
+               // generate a pseudo random number from -3, -6, & -9
+               y =  (sin_lookup (current_time->tm_hour + current_time->tm_min + current_time->tm_sec + 53)) * 1000;
+               time_y_delta = (int) y;
+               time_y_delta = ((time_y_delta % 3) + 1) * 3;
+               time_y_delta = -time_y_delta;
+            }
+         }
+
+         time_x_offset += time_x_delta;
+         time_y_offset += time_y_delta;
       }
    }
-
-   if ((time_y_offset + time_y_delta) < 0)
-   {
-      // generate a pseudo random number from 3, 6, & 9
-      y =  (cos_lookup (current_time->tm_hour + current_time->tm_min + current_time->tm_sec + 43)) * 1000;
-      time_y_delta = (int) y;
-      time_y_delta = ((time_y_delta % 3) + 1) * 3;
-   }
-   else
-   {
-      if ((time_y_offset + time_y_delta + 52) >= 168)
-      {
-         // generate a pseudo random number from -3, -6, & -9
-         y =  (sin_lookup (current_time->tm_hour + current_time->tm_min + current_time->tm_sec + 53)) * 1000;
-         time_y_delta = (int) y;
-         time_y_delta = ((time_y_delta % 3) + 1) * 3;
-         time_y_delta = -time_y_delta;
-      }
-   }
-
-   time_x_offset += time_x_delta;
-   time_y_offset += time_y_delta;
 
    // display time hour
    if (clock_24h_style)
@@ -473,6 +572,9 @@ void update_time(PblTm *current_time)
          set_container_image(&time_digits_images[0], BIG_DIGIT_INV_IMAGE_RESOURCE_IDS[current_time->tm_hour / 10], GPoint(time_x_offset, time_y_offset));
          set_container_image(&time_digits_images[1], BIG_DIGIT_INV_IMAGE_RESOURCE_IDS[current_time->tm_hour % 10], GPoint(21 + time_x_offset, time_y_offset));
       }
+
+      layer_remove_from_parent(&time_digits_images[5].layer.layer);
+      bmp_deinit_container(&time_digits_images[5]);
    }
    else
    {
